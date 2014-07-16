1d233a2a601d08f9ecd5dda3e35d8556ab982413
FungusHumungus <fungus.humungus@gmail.com>
2014-07-16 15:38:53 +0100

    Split at word

---
+++ spec/splitterspec.js
@@ -0,0 +1,28 @@
+describe("splitter", function() {
+    var Splitter = require('../lib/splitter');
+
+    it("splits at character count as default", function() {
+
+        // Split at 14 characters for all tags
+        // No options defaults to split at char
+        var splitter = new Splitter(13);
+
+        var a = splitter.split({text: 'ook ook ook', effectiveLength: function() { return 11; }});
+        expect(a).toBe(null);
+
+        var b = splitter.split({text: 'ook ook ook', effectiveLength: function() { return 11; }});
+        expect(b).toBe('oo...'); 
+     });
+
+    it("splits at word when configured to", function() {
+
+        var splitter = new Splitter(13, {default: 'word'});
+
+        var a = splitter.split({text: 'ook ook ook', effectiveLength: function() { return 11; }});
+        expect(a).toBe(null);
+
+        var b = splitter.split({text: 'ook ook ook', effectiveLength: function() { return 11; }});
+        expect(b).toBe('ook ...'); 
+    });
+
+});
